88cea22ec2c17a88337f5bd0db356df3503fe6c7
Mike Kvederis <m_kvederis@msn.com>
2017-10-11 14:52:01 -0700

    Tests added

---
+++ RPS.Test/LogicTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RPS.Test
+{
+    class LogicTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
