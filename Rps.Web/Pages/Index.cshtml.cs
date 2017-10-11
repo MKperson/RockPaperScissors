14bd09968f63c7de45e56c4a5889a49864c98f87
Mike Kvederis <m_kvederis@msn.com>
2017-10-11 15:52:57 -0700

    index changes

--- Rps.Web/Pages/Index.cshtml.cs
+++ Rps.Web/Pages/Index.cshtml.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rps.Library;
 
 namespace Rps.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public string PlayerName { get; set; }
         public void OnGet()
         {
 
+        }
+        public IActionResult OnPost()
+        {
+            var game = new RpsLogic();
+            return Page();
         }
     }
 }
