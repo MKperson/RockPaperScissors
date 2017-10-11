02ab3d64adc35bf9ce615eec9e64a0bf9515c5aa
Mike Kvederis <m_kvederis@msn.com>
2017-10-11 14:31:07 -0700

    new files

---
+++ Rps.Library/RpsLogic.cs
@@ -0,0 +1,45 @@
+﻿/* Written by Brian Bird    *
+ * 4/27/16                  *
+ * Revised 10/10/17         */
+
+using System;
+
+namespace Rps.Library
+{
+
+    public enum handShape { rock, paper, scissors }
+
+    public class RpsLogic
+    {
+        Random rand = new Random();
+        handShape machineHand;
+
+        public string PlayerName { get; set; }
+
+        /// <summary>
+        /// Randomly chooses a number to represent a hand position.
+        /// </summary>
+        /// <returns>1 for rock, 2 for paper, 3 for scissors.</returns>
+        public handShape ChooseHand()
+        {
+            machineHand = (handShape)rand.Next(3);
+            return machineHand;  // produces random 0 through 2
+        }
+
+
+        // does the choice entered by the user beat the random (android) one?
+        public string WhoWon(string hand)
+        {
+            // Convert the user's hand shape choice to an enum
+            handShape playerHand = (handShape)Enum.Parse(typeof(handShape), hand.ToLower());
+            if (playerHand == machineHand)
+                return "Tie";
+            else if ((playerHand == handShape.rock && machineHand == handShape.scissors) ||
+                     (playerHand > machineHand && !(machineHand == handShape.rock && playerHand == handShape.scissors)))
+                return PlayerName + " won!";
+            else
+                return "Machine won";
+        }
+    }
+}
+
